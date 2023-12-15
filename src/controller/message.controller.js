2dc5dadf663cb1d34ef9f2996bfeac001b1ee208
saransh619 <saranshpachhai@gmail.com>
2023-12-15 22:53:12 +0545

    database connectivity issue tested

--- src/controller/message.controller.js
+++ src/controller/message.controller.js
@@ -1,10 +1,14 @@
-// import Message from '../model/message.model'
-const Message = require('../model/message.model.js')
+import Message from '../model/message.model.js';
 
 const getMessage = async (req, res) => {
-     const message = await Message.find({});
-     res.send(200).json(message);
-};
+    try {
+      const messages = await Message.find({});
+      res.status(200).json(messages);
+    } catch (error) {
+      console.error('Error fetching messages:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  };
 
 const putMessage = async (req, res) => {
     res.send("PUT request received");
